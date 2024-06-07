e181bbf37d35ec69b343f1cba99fac727e66780a
Peter <popazovpeta@gmail.com>
2024-06-07 17:56:05 +0300

    [server] build error fic

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://e-commerce-api-production-691a.up.railway.app/";
 export const ACCESS_TOKEN_NAME = "access_token";
